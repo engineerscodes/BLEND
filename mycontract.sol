a03172d4d3bee36de5202633ff4ad255a20a3e94
NAVEEN <68312849+engineerscodes@users.noreply.github.com>
2022-01-31 20:05:40 +0530

    Update mycontract.sol

--- mycontract.sol
+++ mycontract.sol
@@ -24,3 +24,20 @@ contract mycontract{
 
 
 }
+
+contract mybool{
+
+   bool public myvar;
+   int8 public newInts;
+   uint8  public neg;
+   function setbool() public{
+       myvar=true;
+   }
+   
+   function setInt(int8 newI ) public{
+        newInts=newI;
+   }
+   function setNeg(uint8 newNeg) public{
+       neg=newNeg;
+   }
+}
